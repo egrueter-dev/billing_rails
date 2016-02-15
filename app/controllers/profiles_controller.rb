2c2459417ebe1de33af1bea00cf4efe647718588
Erik Grueter <erikgrueter@gmail.com>
2016-02-15 10:59:30 -0500

    Add more stripe boilerplate

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,6 +4,16 @@ class ProfileController < ApplicationController
   end
 
   def edit
+    require "stripe"
+    Stripe.api_key = "sk_test_PTVRgvgpatfQZVMkHSVlTqLs"
+
+    account = Stripe::Account.retrieve("acct_157VxdKuRisrmOjU")
+    account.external_accounts.create(:external_account => "btok_7udS4m3NKxUHgT")
+    # Update bank account
+    # Updae company name
+    # Update First, Last Name
+    # Add company logo? -- later
+
   end
 
   def update
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,4 @@
 class WelcomeController < ApplicationController
-
   def index
-
   end
-
-
-
-
 end
